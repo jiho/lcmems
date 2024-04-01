4846e17b38094efbab909158d7b1b913f12e5687
Jean-Olivier Irisson <irisson@normalesup.org>
2024-04-01 22:12:06 +0000

    With with(open(...)) when reading files

--- lcmems/open.py
+++ lcmems/open.py
@@ -18,7 +18,11 @@ def open_dataset(path):
     # TODO check that there are as many index files are directories = that the index is complete/up to date
 
     # read all index files
-    index = [pickle.load(open(f,'rb')) for f in index_files]
+    def read_index(f):
+        with open(f, 'rb') as fh:
+            ds = pickle.load(fh)
+        return(ds)
+    index = [read_index(f) for f in index_files]
 
     # concatenate them into one index for the whole dataset
     ds = xr.concat(index, dim='time')
