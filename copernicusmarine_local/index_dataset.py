675bd4d3ebdec58805e00baeba23561adf459bd1
Jean-Olivier Irisson <irisson@normalesup.org>
2024-04-01 15:59:34 +0000

    Allow to skip already indexed directories

--- copernicusmarine_local/index_dataset.py
+++ copernicusmarine_local/index_dataset.py
@@ -4,14 +4,15 @@ import pickle as pkl
 
 # path = '/remote/complex/home/share/cmems_new/OCEANCOLOUR_GLO_BGC_L3_MY_009_107/c3s_obs-oc_glo_bgc-reflectance_my_l3-multi-4km_P1D_202303'
 
-def index_dataset(path):
+def index_dataset(path, force=False):
     """Create an xarray index of a dataset
     
     For each year, open the netCDF files as xarrays and store their structure in
-    a pickle file, that can then be easily loaded
+    a pickle file, that can then be easily loaded.
     
     Args:
         path (str): path to the dataset. See list_datasets() to discover them.
+        force (boolean): if True, recreate the index file even if it exists.
         
     Returns:
         'True' upon success. A pickle file is written per sub-directory of
@@ -23,12 +24,16 @@ def index_dataset(path):
     years.sort()
 
     for y in years:
-        print('indexing ' + y)
-        ds = xr.open_mfdataset(path+'/'+y+'/*/*.nc',
-                               decode_cf=False, decode_times=False,
-                               join='exact',coords='minimal', compat='override')
-        ds = xr.decode_cf(ds)
-        with open(path+'/'+y+'_xarray.pickle', 'wb') as file:
-            pkl.dump(ds, file)
+        index_file = path+'/'+y+'_xarray.pickle'
+        if (os.path.exists(index_file)) & (not force):
+            print('skipping ' + y)
+        else:
+            print('indexing ' + y)
+            ds = xr.open_mfdataset(path+'/'+y+'/*/*.nc',
+                                   decode_cf=False, decode_times=False,
+                                   join='exact',coords='minimal', compat='override')
+            ds = xr.decode_cf(ds)
+            with open(index_file, 'wb') as file:
+                pkl.dump(ds, file)
     
     return(True)
